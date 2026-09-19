1125480fae46b66bb721fd42ccc7380b05ec10ed
kat <kathleennirwasita@gmail.com>
2026-09-19 16:36:52 +0700

    Update API endpoint for fetching geek jokes

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.command("/slackernot-geekout", async ({ ack, respond }) => {
   await ack();
 
   try {
-    const response = await axios.get("https://geek-jokes.sameerkumar.website/api?format=json");
+    const response = await axios.get("https://geek-jokes.sameerkumar.website/api");
     await respond({ text: `Geek Joke:\n${response.data.joke}` });
   } catch (err) {
     await respond({ text: "Failed to fetch a geek joke." });
